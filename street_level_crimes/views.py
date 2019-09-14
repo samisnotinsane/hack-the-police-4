2b79dfcb025a100cd6a1f9adac38f75241f2ad3b
samisnotinsane <sameen11@hotmail.com>
2019-09-14 18:34:20 +0100

    Crime response parsed into list and view.

--- street_level_crimes/views.py
+++ street_level_crimes/views.py
@@ -3,10 +3,12 @@ from django.http import HttpResponse
 from django.template import loader
 
 import requests
+from . import services
 
 def index(request):
-    response = requests.get('https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-01')
-    crimedata = response.json()
+    # response = requests.get('https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-01')
+    # crimedata = response.json()
+    crimes_list = services.get_street_crimes(latitude='52.629729', longitude='-1.131592', date='2019-01')
     return render(request, 'street_level_crimes/index.html', {
-        'crimedata': crimedata
+        'crimes_list': crimes_list
     })
