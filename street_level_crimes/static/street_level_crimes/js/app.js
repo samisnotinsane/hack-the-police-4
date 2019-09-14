89a3f01e7fc8379a87cad62d151632336387eb83
samisnotinsane <sameen11@hotmail.com>
2019-09-14 20:20:49 +0100

    Adding ESRI map to crime_map.html

--- street_level_crimes/static/street_level_crimes/js/app.js
+++ street_level_crimes/static/street_level_crimes/js/app.js
@@ -1,2 +1,21 @@
 // App level, global js script
 console.log("app.js is working")
+
+// Arc GIS Map init
+require([
+    "esri/Map",
+    "esri/views/MapView",
+  ], function(Map, MapView) {
+
+    var map = new Map({
+      basemap: "topo-vector"
+    });
+
+    var view = new MapView({
+      container: "viewDiv",
+      map: map,
+      center: [-118.80543,34.02700],
+      zoom: 13
+    });
+
+  });
