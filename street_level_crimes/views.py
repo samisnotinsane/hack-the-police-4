1f3bf06c88d5c2ff3c122b28e55dd984399cb037
samisnotinsane <sameen11@hotmail.com>
2019-09-14 16:59:16 +0100

    Adding ability to dump JSON into crime view

--- street_level_crimes/views.py
+++ street_level_crimes/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+import requests
+
 def index(request):
-    return render(request, 'street_level_crimes/index.html', {})
+    response = requests.get('https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-01')
+    crimedata = response.json()
+    return render(request, 'street_level_crimes/index.html', {
+        'crimedata': crimedata
+    })
